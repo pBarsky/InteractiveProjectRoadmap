a9daeb07d0a34c91e45f804ce69bbc44e061a208
pBarsky <patrykbaranski12@gmail.com>
2021-05-02 22:54:53 +0200

    front

---
+++ client-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>Interactive Project Roadmap</header>
+    </div>
+  )
+}
+
+export default App
