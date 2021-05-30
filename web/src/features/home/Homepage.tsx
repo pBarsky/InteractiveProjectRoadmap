07874fcd961ca2d5b27dd134b835db81ed26dd27
pBarsky <patrykbaranski12@gmail.com>
2021-05-30 16:30:52 +0200

    CA-32 Przekierowanie usera na dashboard z homepage

--- web/src/features/home/Homepage.tsx
+++ web/src/features/home/Homepage.tsx
@@ -1,16 +1,19 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Button, Container, Header, Segment } from 'semantic-ui-react';
 import routes from '../../app/common/routing/routes';
 import defaultDict from '../../app/dictionaries/defaultDict';
 import { useStore } from '../../app/stores/store';
-/*
- * strona główna, napis z przywitaniem i link do logowania/rejestracji
- */
 
 const Homepage = () => {
-  const { authStore: userStore } = useStore();
+  const {
+    authStore: { isLoggedIn }
+  } = useStore();
+
+  if (isLoggedIn) {
+    return <Redirect to={routes.user.dashboard} />;
+  }
 
   return (
     <Segment textAlign='center' vertical>
@@ -23,21 +26,9 @@ const Homepage = () => {
             {defaultDict.common.appName}
           </Header>
         </Segment>
-        {userStore.isLoggedIn
-          ? (
-          <Button
-            as={Link}
-            to={routes.user.dashboard}
-            size='huge'
-            color='black'>
-            {defaultDict.common.welcomeButton}
-          </Button>
-            )
-          : (
-          <Button as={Link} to={routes.auth.login} size='huge' color='black'>
-            {defaultDict.forms.buttons.login.text}
-          </Button>
-            )}
+        <Button as={Link} to={routes.auth.login} size='huge' color='black'>
+          {defaultDict.forms.buttons.login.text}
+        </Button>
       </Container>
     </Segment>
   );
