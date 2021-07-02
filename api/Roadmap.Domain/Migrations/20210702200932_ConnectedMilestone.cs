bf57ecf9dc67bde8cc1d5c1c003169df70e28cd8
pBarsky <patrykbaranski12@gmail.com>
2021-07-02 23:45:39 +0200

    CA-68 Łączenie milestonów w api

---
+++ api/Roadmap.Domain/Migrations/20210702200932_ConnectedMilestone.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Roadmap.Domain.Migrations
+{
+    public partial class ConnectedMilestone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConnectedToId",
+                table: "Milestones",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Milestones_ConnectedToId",
+                table: "Milestones",
+                column: "ConnectedToId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Milestones_Milestones_ConnectedToId",
+                table: "Milestones",
+                column: "ConnectedToId",
+                principalTable: "Milestones",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Milestones_Milestones_ConnectedToId",
+                table: "Milestones");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Milestones_ConnectedToId",
+                table: "Milestones");
+
+            migrationBuilder.DropColumn(
+                name: "ConnectedToId",
+                table: "Milestones");
+        }
+    }
+}
