00a5514fa444c423ca66fa1922778b647dc109aa
pBarsky <patrykbaranski12@gmail.com>
2021-07-15 12:26:25 +0200

    CA-75 nie ma już debugowych wartości na widoku 😅

--- web/src/features/milestone/MilestoneFlowNode.tsx
+++ web/src/features/milestone/MilestoneFlowNode.tsx
@@ -8,49 +8,32 @@ const MilestoneFlowNode = (data: FlowElement<Milestone>): JSX.Element => {
 	const milestoneFlowElement = data.data;
 	return (
 		<>
-			<pre style={{ fontWeight: 'bold' }}>
-				{'\n'}connectedToSourceHandleId: {milestoneFlowElement?.connectedToSourceHandleId}
-				{'\n'}connectedToTargetHandleId: {milestoneFlowElement?.connectedToTargetHandleId}
-				{'\n'}connectedToId: {milestoneFlowElement?.connectedToId}
-			</pre>
-			<>
-				<Handle
-					type='target'
-					position={Position.Left}
-					className={styles.handle}
-					id={HandleId.Left.toString()}
-				/>
-				<span>Lewa T</span>
-			</>
-			<>
-				<Handle
-					type='source'
-					position={Position.Left}
-					className={styles.handle}
-					id={HandleId.Left.toString()}
-				/>
-				<span style={{ marginLeft: 10 }}>Lewa S</span>
-			</>
+			<Handle
+				type='target'
+				position={Position.Left}
+				className={styles.handle}
+				id={HandleId.Left.toString()}
+			/>
+			<Handle
+				type='source'
+				position={Position.Left}
+				className={styles.handle}
+				id={HandleId.Left.toString()}
+			/>
 
 			<MilestoneCard milestone={milestoneFlowElement!} />
-			<>
-				<Handle
-					type='target'
-					position={Position.Right}
-					className={styles.handle}
-					id={HandleId.Right.toString()}
-				/>
-				<span>Prawa T</span>
-			</>
-			<>
-				<Handle
-					type='source'
-					position={Position.Right}
-					className={styles.handle}
-					id={HandleId.Right.toString()}
-				/>
-				<span style={{ marginLeft: 10 }}>Prawa S</span>
-			</>
+			<Handle
+				type='target'
+				position={Position.Right}
+				className={styles.handle}
+				id={HandleId.Right.toString()}
+			/>
+			<Handle
+				type='source'
+				position={Position.Right}
+				className={styles.handle}
+				id={HandleId.Right.toString()}
+			/>
 		</>
 	);
 };
